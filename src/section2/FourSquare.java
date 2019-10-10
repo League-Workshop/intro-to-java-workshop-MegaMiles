5842cf8a40b80c1fcea0984a39cdb953bdd10f95
League Student <league@iMac-34.attlocal.net>
2019-10-09 18:25:57 -0700

    4 square

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -4,30 +4,37 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
-	
+
 	// 2. Create a new Robot
+	Robot jeff = new Robot();
 
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+		jeff.setSpeed(100);
+		jeff.penDown();
 		// 5. Set the pen width to 5
-
+		jeff.setPenWidth(5);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
+		for (int i = 0; i < 4; i++) {
 
 			// 7. Set the pen color to random
-	
+			jeff.setRandomPenColor();
 			// 1. Call the drawSquare() method
-	
+			drawSquare();
 			// 8. Turn the robot 90 degrees to the right
+
+		}
 
 	}
 
-	
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
-		
-		
+		for (int i = 0; i < 4; i++) {
+			jeff.move(50);
+			jeff.turn(90);
+		}
+
 	}
 
 	public static void main(String[] args) {
@@ -35,6 +42,3 @@ public class FourSquare {
 	}
 
 }
-
-
-
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,21 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot jeff = new Robot();
+	jeff.setSpeed(100);
+	jeff.penDown();
+	jeff.setRandomPenColor();
 	
+	
+	for (int i = 0; i <4; i++) {
 		
-		
+		jeff.turn(120);
+		jeff.move(100);
+		jeff.turn(120);
+		jeff.move(100);
+		jeff.turn(120);
+		jeff.move(100);
+			
+	}
 	}
 }
