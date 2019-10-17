c6ea2ad6405712e8648e59fae67a57b84a38d0e5
League Student <league@iMac-34.attlocal.net>
2019-10-16 18:29:16 -0700

    candy crush

---
+++ src/section3/helloworld.java
@@ -0,0 +1,50 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class helloworld {
+	public static void main(String[] args) {
+		System.out.println("");
+		JOptionPane.showMessageDialog(null,
+				"
+	
+	
+	
+		String str = ":]";
+		System.out.println(str);
+	String str2=JOptionPane.showInputDialog("YOURE STUPID!!!!!!!");
+	System.out.println(str2);
+	System.out.println("hello!"+str2+"hello!");	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+
+}
